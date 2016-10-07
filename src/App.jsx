3f1e4db29564bf00247ac138da1ab5c5acf8730b
Jen Yee <jenyee1022@yahoo.com>
2016-10-07 18:49:48 +0000

    refactored push message function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import NumberUsers from './NumberUsers.jsx';
 
 
 const App = React.createClass({
-  //set the state of the parent
   getInitialState: function() {
     let data = {
       currentUser: {name: "Anonymous"},
@@ -21,14 +20,13 @@ const App = React.createClass({
     return {data: data};
   },
 
-
-
   //send the message to the websocket server
   onSendMessage: function(user, text) {
     let currentUser = this.state.data.currentUser.name;
     if (!user) {
       user = "Anonymous";
     }
+    //if the user changed their name, send a notification message
     if (currentUser !== user) {
       let newMsg = {
         username: user,
@@ -37,44 +35,47 @@ const App = React.createClass({
       }
       this.socket.send(JSON.stringify(newMsg));
     }
-
+    //always send the message content to the server
     let newMsg = {
       username: user,
       content: text,
       type: "postMessage"
     }
     this.socket.send(JSON.stringify(newMsg));
-
-
   },
 
   pushMessage: function (message) {
-    this.state.data.messages.push(message);
-    let newUser =  message.username;
-    this.state.data.currentUser.name = newUser;
-    return this.state.data;
+    let pushedMsgs = this.state.data.messages.push(message);
+    // let newUser =  message.username;
+    // this.state.data.currentUser.name = newUser;
+    // return this.state.data;
+    return pushedMsgs;
   },
 
   //after mounting
   componentDidMount: function() {
-    console.log("componentDidMount <App />");
-
     //creates the pipeline to the other server
     this.socket = new WebSocket("ws://localhost:4000/socketserver");
     this.socket.onopen = function(event) {
       console.log("Connected to websocket server");
     }
-
     //message received from the server
     this.socket.onmessage = (event) => {
       let parsedMsg = JSON.parse(event.data);
+      console.log(parsedMsg);
       switch(parsedMsg.type) {
+        //same user
         case "incomingMessage":
-          let newStateSameUser = this.pushMessage(parsedMsg);
+          //let newStateSameUser = this.pushMessage(parsedMsg);
+          let msgArr1 = this.pushMessage(parsedMsg);
+          let newStateSameUser = Object.assign({}, this.state.data, {name: parsedMsg.username}, msgArr1);
           this.setState({data: newStateSameUser});
           break;
+        //different user
         case "incomingNotification":
-          let newStateDiffUser = this.pushMessage(parsedMsg);
+          let msgArr2 = this.pushMessage(parsedMsg);
+          //let newStateDiffUser = this.pushMessage(parsedMsg);
+          let newStateDiffUser = Object.assign({}, this.state.data, {name: parsedMsg.username}, msgArr2);
           this.setState({data: newStateDiffUser});
           break;
         case "colorAssigned":
@@ -86,10 +87,7 @@ const App = React.createClass({
           let userCount = Object.assign({}, this.state.data, {numUsers: parsedMsg});
           this.setState({data: userCount});
       }
-
-
     }
-
   },
 
   render: function() {
@@ -102,12 +100,10 @@ const App = React.createClass({
           />
         </nav>
         <MessageList
-          //send the message array to MessageList
           messages={this.state.data.messages}
           color={this.state.data.color.color}
         />
         <ChatBar
-          //send the current user to the footer
           currentUser={this.state.data.currentUser}
           //this callback receives the message after hitting the enter key in the chatbar
           onSendMessage={this.onSendMessage}
